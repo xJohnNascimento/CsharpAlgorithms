d5c738a3a901e7b521a888250d3f93227bd492b1
xJohnNascimento <xjohn.nascimento@gmail.com>
2023-01-02 18:10:49 -0300

    Initial Commit

---
+++ Csharp-Algorithms/Csharp-Algorithms/InsertionSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpAlgorithms
+{
+    public class InsertionSort
+    {
+        public int[] SortArray(int[] array, int length)
+        {
+            for (int i = 1; i < length; i++)
+            {
+                var key = array[i];
+                var flag = 0;
+                for (int j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (key < array[j])
+                    {
+                        array[j + 1] = array[j];
+                        j--;
+                        array[j + 1] = key;
+                    }
+                    else
+                    {
+                        flag = 1;
+                    }
+                }
+            }
+            return array;
+        }
+    }
+}
