0d4a31b1645bba3694bd4c88ef8e13281af89363
xJohnNascimento <xjohn.nascimento@gmail.com>
2023-03-15 13:19:22 -0300

    Add tests to RomanToInt and change version to .net8

--- CsharpAlgorithms/CsharpAlgorithms.Test/Exercises/RomanToIntExerciseTest.cs
+++ CsharpAlgorithms/CsharpAlgorithms.Test/Exercises/RomanToIntExerciseTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CsharpAlgorithms.Exercises.Easy;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,18 @@ using System.Threading.Tasks;
 
 namespace CsharpAlgorithms.Test.Exercises
 {
-    internal class RomanToIntExerciseTest
+    public class RomanToIntExerciseTest
     {
+        [Fact]
+        public void RomanToInt_Success()
+        {
+            var romanToIntConverter = new RomanToIntExercise();
+
+            var romanNumber = "XIX";
+
+            var result = romanToIntConverter.RomanToInt(romanNumber);
+
+            Assert.Equal(19, result);
+        }
     }
 }
