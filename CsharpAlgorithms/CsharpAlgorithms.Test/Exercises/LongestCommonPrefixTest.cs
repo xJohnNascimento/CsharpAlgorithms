b017fedc8a1182fe7b470ca2a58cfb2a93aa5e89
xJohnNascimento <xjohn.nascimento@gmail.com>
2023-03-15 23:42:44 -0300

    Add longestCommonPrefix exercise and test

---
+++ CsharpAlgorithms/CsharpAlgorithms.Test/Exercises/LongestCommonPrefixTest.cs
@@ -0,0 +1,24 @@
+﻿using CsharpAlgorithms.Exercises.Easy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpAlgorithms.Test.Exercises
+{
+    public class LongestCommonPrefixTest
+    {
+        [Fact]
+        public void LongestCommonPrefix_Success()
+        {
+            var longestCommonPrefix = new LongestCommonPrefixExercise();
+
+            string[] strings = { "flower", "fl", "flight" };
+
+            var result = longestCommonPrefix.LongestCommonPrefix(strings);
+
+            Assert.Equal("fl", result);
+        }
+    }
+}
---
+++ CsharpAlgorithms/CsharpAlgorithms/Exercises/Easy/LongestCommonPrefixExercise.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpAlgorithms.Exercises.Easy
+{
+    public class LongestCommonPrefixExercise
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            var result = "";
+            int currentLetter = 0;
+
+            if (strs.Length == 0 || strs[currentLetter].Length == 0)
+            {
+                return result;
+            }
+
+            char letter = strs[0][currentLetter];
+
+            for(int i = 0; i < strs.Length; i++)
+            {
+                if (strs[i] == string.Empty || strs[i].Length - 1 < currentLetter)
+                {
+                    break;
+                }
+
+                if (strs[i][currentLetter] != letter)
+                {
+                    break;
+                }
+
+                if (i == strs.Length - 1)
+                {
+                    result += strs[i][currentLetter];
+
+                    i = 0;
+                    currentLetter++;
+
+                    if (strs[i].Length - 1 < currentLetter)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        letter = strs[i][currentLetter];
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
